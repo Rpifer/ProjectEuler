5f421d615a49f7fea24794a86f005512bbf0db09
Pifer <rpifer@udel.edu>
2020-03-08 11:52:21 -0400

    Problem 1

--- ProblemRunner.py
+++ ProblemRunner.py
@@ -1,7 +1,11 @@
 import importlib
 
 if __name__ == '__main__':
-    test_number = 1
+    debug = False
+
+    test_number = 2
+
     module = importlib.import_module('Problems.Problem%s' % test_number)
+    module.debug = debug
     result = module.test_problem()
     print('Answer:\n%s' % result)
--- Problems/Problem1.py
+++ Problems/Problem1.py
@@ -1,18 +1,24 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-
+debug = False
 
 def test_problem():
     result = 0
     multiple = 1
 
     while multiple * 3 < 1000:
+        if debug:
+            print(result)
+            print(multiple)
         result += multiple * 3
         multiple += 1
 
     multiple = 1
     while multiple * 5 < 1000:
+        if debug:
+            print(result)
+            print(multiple)
         if multiple % 3 != 0:
             result += multiple * 5
         multiple += 1
